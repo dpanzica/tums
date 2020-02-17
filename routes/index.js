7a29ea911deced5edfbdf4835b62a9935f100d6f
Damon Panzica <dpanzica@deapul.mail.edu>
2020-02-16 23:54:42 -0600

    updated about

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,9 @@
-const routeResponseMap = {
-    "/": "views/index.html",
-        "/about": "views/about.html",
-        "/contact": "views/contact.html",
-        "/error": "views/error.html"
-};
-	"/about": "<h1> About Simple Application</h1>",
-	"/contact": "<h1> Contact Us </h1>",
-	"/error": "<h1> Sorry - cannot find what you are looking for. </h1>"
-};
+var express = require('express');
+var router = express.Router();
 
-var http = require('http');
-var fs = require('fs');
-var port = 3015;
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    if (routeResponseMap[req.url]){
-        fs.readFile(routeResponseMap[req.url], (error, data) => {
-                res.write(data);
-                res.end();
-                });
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
-			res.end(routeResponseMap[req.url]);
-		} else {
-			res.end('Welcome To First Nodejs Application \n' );
-		}
-
-    res.end('Welcome To First Nodejs Application \n' );
-    console.log('Processed request for '+ req.url);
-}).listen(port);
-console.log('Web application server running ...');
+module.exports = router;
