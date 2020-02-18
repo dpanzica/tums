fde5b88c3baeff1a8428c4791c40809db17ebe6d
Damon Panzica <dpanzica@deapul.mail.edu>
2020-02-18 10:03:18 -0600

    Created project code

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
 var contactRouter = require('./routes/contact');
+var layouts = require("express-ejs-layouts");
 
 var app = express();
 
@@ -25,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
-app.use('/contact', contactRouter);
+app.use(layouts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.render('about', { title: 'AboutsampleApp' });
 });
 
 module.exports = router;
