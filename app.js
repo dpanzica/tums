d368839d827f5da78647bb8d7c59ebc8c67600cc
Damon Panzica <dpanzica@deapul.mail.edu>
2020-02-19 13:52:05 -0600

    created project code

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var layouts = require('express-ejs-layouts');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
 var contactRouter = require('./routes/contact');
-var layouts = require("express-ejs-layouts");
 
 var app = express();
 
@@ -21,12 +22,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(layouts);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
-app.use(layouts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
