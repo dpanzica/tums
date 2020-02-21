9d7360121becaaf409b949949bc12399bcd9a132
Damon Panzica <dpanzica@deapul.mail.edu>
2020-02-20 20:20:59 -0600

    created code

--- tums/app.js
+++ tums/app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var usersRouter = require('./routes/about')
+var aboutRouter = require('./routes/about');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/about', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
