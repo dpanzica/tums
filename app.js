dead6b10c8646cf77b7a4536509067fb4fe4d706
Damon Panzica <dpanzica@deapul.mail.edu>
2020-02-20 20:00:17 -0600

    created code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var layouts = require('express-ejs-layouts');
-var ejs = require('ejs');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
