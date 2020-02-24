b2d86865af4afe004730b77055f88583556b9411
Damon Panzica <dpanzica@deapul.mail.edu>
2020-02-24 13:34:00 -0600

    created code

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.render('about', { title: 'About SampleApp' });
 });
 
 module.exports = router;
