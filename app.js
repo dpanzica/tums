8b5f81c0828e2a8c14cd96d2ffcfe3ddad22f75d
Damon Panzica <dpanzica@deapul.mail.edu>
2020-02-24 14:14:13 -0600

    created code

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(layouts);
 
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
--- routes/dyna.js
+++ routes/dyna.js
@@ -1,7 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET about page. */
+/* GET dyna page. */
 router.get('/', function(req, res, next) {
-  res.render('dyna', { passeddata: 'SampleData' });
+let booklist = [
+        {
+            title: 'Tightrope - Americans Reaching for Hope',
+            author: 'Nicholas D. Kristof and Sheryl WuDunn',
+            publishedAt: new Date('2020-01-20'),
+        },
+        {
+            title: 'Dear Edward',
+            author: 'Ann Napolitano',
+            publishedAt: new Date('2019-12-18'),
+        },
+        {
+            title: 'I Am the Lorax',
+            author: ' Courtney Carbone',
+            publishedAt: new Date('2020-05-17'),
+        }
+    ]
+
+  res.render('dyna', { books: booklist});
 });
+
+module.exports = router;
+
