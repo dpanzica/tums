6ef4655ab0a128b5a4d7f3873298d7def00e3e56
Damon Panzica <dpanzica@deapul.mail.edu>
2020-02-26 17:56:33 -0600

    created code

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let query = "SELECT product_id, productname, productimage, status, saleprice from product"; 
+  /*let query = "SELECT product_id, productname, productimage, status, saleprice from product"; 
 
   // execute query
   db.query(query, (err, result) => {
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 			res.redirect('/');
 		}
 	res.render('index', {title: 'tums2u', products: result });
- 	});
+ 	});*/
 });
 
 module.exports = router;
