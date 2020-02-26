1724d7e04b68778fc08d68c87dd5f6b9e93b9623
Damon Panzica <dpanzica@deapul.mail.edu>
2020-02-26 14:22:30 -0600

    created code

--- routes/dyna.js
+++ routes/dyna.js
@@ -5,19 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 let booklist = [
         {
-            title: 'Tightrope - Americans Reaching for Hope',
-            author: 'Nicholas D. Kristof and Sheryl WuDunn',
-            publishedAt: new Date('2020-01-20'),
+            title: 'Tums: Chewy Bites',
+            author: 'A chewable version of the classic Tum. Tastes great.',
+            publishedAt: '4.99',
         },
         {
-            title: 'Dear Edward',
-            author: 'Ann Napolitano',
-            publishedAt: new Date('2019-12-18'),
+            title: 'Tums Ultra-Strength 1000mg',
+            author: 'Tums Ultra strength is for those in need of something more powerful.',
+            publishedAt: '5.99',
         },
         {
-            title: 'I Am the Lorax',
-            author: ' Courtney Carbone',
-            publishedAt: new Date('2020-05-17'),
+            title: 'Emergen Cee',
+            author: 'comes in a four pack and helps you get your dose of vitamin c.',
+            publishedAt: '3.99',
         }
     ]
 
