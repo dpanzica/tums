799451a06f0fa9112b79f71ed7c61f0ac53a8b70
Damon Panzica <dpanzica@deapul.mail.edu>
2020-03-02 13:46:52 -0600

    created code

--- routes/customer.js
+++ routes/customer.js
@@ -15,9 +15,9 @@ module.exports = router;
 // Route Save Customer Registration
 // ==================================================
 router.post('/', function(req, res, next) {
-  let insertquery = "INSERT INTO customer(firstname, lastname, email, phone, address1, address2, city, state, zip, addlnotes, rewardpoints) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"; 
+  let insertquery = "INSERT INTO customer(firstname, lastname, email, phone, address1, address2, city, state, zip, addlnotes, rewardpoints,username, password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?)"; 
 
-	db.query(insertquery,[req.body.firstname, req.body.lastname, req.body.email,req.body.phone,req.body.address1, req.body.address2, req.body.city, req.body.state, req.body.zip, req.body.addlnotes, req.body.rewardpoints],(err, result) => {
+	db.query(insertquery,[req.body.firstname, req.body.lastname, req.body.email,req.body.phone,req.body.address1, req.body.address2, req.body.city, req.body.state, req.body.zip, req.body.addlnotes, req.body.rewardpoints,req.body.username, req.body.password],(err, result) => {
 	if (err) {
 			res.render('error');
 			} else {
