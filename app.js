45ea78a220284541b7827d0819d494140609fc93
Damon Panzica <dpanzica@deapul.mail.edu>
2020-03-08 23:56:22 -0500

    created code

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const db = mariadb.createConnection({host: 'eagle.cdm.depaul.edu', user: 'dpanzi
 db.connect((err) => {
   if (err) {
     console.log("not connected due to error: " + err);
+	res.render('error');
   } else
 	{
     console.log("connected to db");
@@ -20,9 +21,6 @@ db.connect((err) => {
 });
 
 global.db = db;
-
-const cart = [];
-global.cart = cart;
 
 
 var indexRouter = require('./routes/index');
@@ -32,12 +30,13 @@ var contactRouter = require('./routes/contact');
 var dynaRouter = require('./routes/dyna');
 var productRouter = require('./routes/product');
 var categoryRouter = require('./routes/category');
-var privacyRouter = require('./routes/privacy');
-var helpRouter = require('./routes/help');
 var catalogRouter = require('./routes/catalog');
 var customerRouter = require('./routes/customer');
 
+
 var app = express();
+
+app.use(session({secret: 'Ckajsdfaskdjfhcret'}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -56,13 +55,11 @@ app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
 app.use('/dyna', dynaRouter);
-app.use('/privacy', privacyRouter);
-app.use('/help', helpRouter);
 app.use('/product', productRouter);
 app.use('/category', categoryRouter);
 app.use('/catalog', catalogRouter);
 app.use('/customer', customerRouter);
-app.use(session({secret: 'TumsSecret'}));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
